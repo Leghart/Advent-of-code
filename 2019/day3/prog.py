61894c86368625cc758481cf7c30f56334f38ebe
Dawid Sielużycki <leghart@wp.pl>
2021-08-02 15:14:41 +0200

    day3 - part2

--- 2019/day3/prog.py
+++ 2019/day3/prog.py
@@ -25,7 +25,32 @@ def make_path(wire):
     return moved_set
 
 
+def count_steps(stop_value, wire):
+    ctr = 0
+    wire = wire.split(",")
+    moved_set = set()
+    start = (0, 0)
+    for order in wire:
+        drt, val = order[0], int(order[1:])
+        for v in range(val):
+            start = move(start, drt)
+            if stop_value not in moved_set:
+                moved_set.add(start)
+                ctr += 1
+            else:
+                return ctr
+    return ctr
+
+
 path1 = make_path(wire1)
 path2 = make_path(wire2)
-manhatann = min([sum([abs(x) for x in vec]) for vec in path1 & path2])
-print(manhatann)
+intersection_points = path1 & path2
+manhatann = min([sum([abs(x) for x in vec]) for vec in intersection_points])
+print("Part 1: ", manhatann)
+
+out = []
+stop_values = list(intersection_points)
+wires_set = (wire1, wire2)
+for stop in stop_values:
+    out.append(sum([count_steps(stop, wire) for wire in wires_set]))
+print("Part 2: ", min(out))
