1b9d4537313f405784acd7fc174bc1eaec55b917
Leghart <leghart@wp.pl>
2022-12-07 21:01:51 +0100

    2022: day7

--- 2022/day7/main.py
+++ 2022/day7/main.py
@@ -1,59 +1,71 @@
+import os
+
 with open("data.txt", "r") as f:
     data = f.read().splitlines()
 
-b = []
-for i in data:
-    a = i.split(" ")
-    if a[0] == "dir":
-        b.append(a[1])
-print("dirs len: ", len(b))
-
 tree = {}
-current_dir = "/"
+
 for line in data:
     if line.startswith("$"):
-        line_command = line.split(" ")
-        if len(line_command) == 3 and line_command[2].isalpha():
-            current_dir = line_command[2]
-            tree[current_dir] = {}
+        sign, cmd, *rest = line.split(" ")
 
-        elif line_command[1] == "ls":
+        if cmd == "cd":
+            path = rest[0]
+            if path == "/":
+                current_dir = path
+            else:
+                new_path = os.path.join(current_dir, path)
+                current_dir = os.path.normpath(new_path)
+
+            if current_dir not in tree:
+                tree[current_dir] = {}
+        if cmd == "ls":
             tmp_list = []
 
     else:
         p1, p2 = line.split(" ")
         if p1 == "dir":
-            tmp_list.append(p2)
+            new_path = os.path.join(current_dir, p2)
+            tmp_list.append(new_path)
         else:
             tmp_list.append((p1, p2))
 
         tree[current_dir] = tmp_list
 
 
-# def get_files_sizes(files: list) -> int:
-#     return sum([int(file[0]) for file in files])
+def get_files_sizes(files: list) -> int:
+    return sum([int(file[0]) for file in files])
 
 
-# def get_sizes(dir_content: list):
-#     if all([isinstance(item, tuple) for item in dir_content]):
-#         return get_files_sizes(dir_content)
+def get_sizes(dir_content: list):
+    if all([isinstance(item, tuple) for item in dir_content]):
+        return get_files_sizes(dir_content)
 
-#     tmp = []
-#     for file in dir_content:
-#         if isinstance(file, str):
-#             tmp.append(get_sizes(tree[file]))
-#         else:
-#             tmp.append(get_files_sizes([file]))
+    tmp = []
+    for file in dir_content:
+        if isinstance(file, str):
+            tmp.append(get_sizes(tree[file]))
+        else:
+            tmp.append(get_files_sizes([file]))
 
-#     return sum(tmp)
-
-
-# new_tree = {}
-# for dir, files in tree.items():
-#     new_tree[dir] = get_sizes(files)
-
-# for a, b in new_tree.items():
-#     print(a, b)
+    return sum(tmp)
 
 
-# print("Part 1: ", sum([size for size in new_tree.values() if size <= 100000]))
+new_tree = {dir: get_sizes(files) for dir, files in tree.items()}
+
+print("Part 1: ", sum([size for size in new_tree.values() if size <= 100000]))
+
+max_size = 70_000_000
+required_free_space = 30_000_000
+
+sorted_values = sorted(new_tree.values(), reverse=True)
+unused_space = max_size - sorted_values[0]
+
+
+possible_dirs_to_remove = [
+    size_to_remove
+    for size_to_remove in sorted_values
+    if unused_space + size_to_remove >= required_free_space
+]
+
+print("Part 2: ", min(possible_dirs_to_remove))
