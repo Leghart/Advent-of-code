37833a9a52d6cf4a49705c23e99e068c511960aa
Dawid <dawid.sieluzycki@nokia.com>
2022-12-10 18:03:44 +0100

    2022: day9 (part2)

--- 2022/day9/main.py
+++ 2022/day9/main.py
@@ -8,16 +8,27 @@ class Point:
     def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
+        self.traceback = [(x, y)]
+
+    def move_vec(self, vec: tuple[int, int]):
+        self.x += vec[0]
+        self.y += vec[1]
+
+        if (self.x, self.y) not in self.traceback:
+            self.traceback.append((self.x, self.y))
 
     def move(self, direction: str, distance: int) -> tuple[int, int]:
         if direction == "R":
-            self.y += distance
-        elif direction == "L":
-            self.y -= distance
-        elif direction == "U":
-            self.x -= distance
-        elif direction == "D":
             self.x += distance
+        elif direction == "L":
+            self.x -= distance
+        elif direction == "U":
+            self.y += distance
+        elif direction == "D":
+            self.y -= distance
+
+        if (self.x, self.y) not in self.traceback:
+            self.traceback.append((self.x, self.y))
 
         return (self.x, self.y)
 
@@ -25,83 +36,44 @@ class Point:
         return (self.x, self.y)
 
 
-class Head(Point):
-    ...
+class Rope:
+    def __init__(self, tail_length: int):
+        self.points = [Point(0, 0) for _ in range(tail_length)]
 
+    def move_tail(self, point: Point, prev_point: Point) -> None:
+        dx = prev_point.x - point.x
+        dy = prev_point.y - point.y
 
-class Tail(Point):
-    def __init__(self, x: int, y: int):
-        super().__init__(x, y)
-        self.traceback = []
+        if abs(dx) > 1 or abs(dy) > 1:
+            vec = (dx / max(abs(dx), 1), dy / max(abs(dy), 1))
+            point.move_vec(vec)
 
-    def move(self, direction: str, distance: int) -> None:
-        x, y = super().move(direction, distance)
-        if (x, y) not in self.traceback:
-            self.traceback.append((x, y))
-
-
-class Board:
-    def __init__(self, size: int):
-        self.size = size
-        self.matrix = self.init_board()
-        self.head = Head(size - 1, 0)
-        self.tail = Tail(size - 1, 0)
-        # self.update_board()
-
-    def move_rope(self, direction: str, distance: int):
+    def move_rope(self, direction: str, distance: int) -> None:
         for _ in range(distance):
-            self.head.move(direction, 1)
-            # self.update_board()
+            prev_point = None
+            for point in self.points:
 
-            if (
-                not self.is_tail_touching_head()
-                and not self.is_tail_in_the_same_row_or_col()
-            ):
-                self.correct_tail_course(direction)
+                if prev_point is None:
+                    point.move(direction, 1)
 
-            if not self.is_tail_touching_head():
-                self.tail.move(direction, 1)
-
-            # self.update_board()
-            # print(self)
-
-    def correct_tail_course(self, direction: str):
-        if direction in ("U", "D"):
-            self.tail.y = self.head.y
-        else:
-            self.tail.x = self.head.x
-
-    def init_board(self) -> list[list[int]]:
-        return [["." for _ in range(self.size)] for _ in range(self.size)]
-
-    def update_board(self) -> None:
-        for row in range(self.size):
-            for col in range(self.size):
-                if (row, col) == self.head.curr_pos():
-                    self.matrix[row][col] = "H"
-                elif (row, col) == self.tail.curr_pos():
-                    self.matrix[row][col] = "T"
                 else:
-                    self.matrix[row][col] = "."
+                    self.move_tail(point, prev_point)
 
-    def is_tail_touching_head(self) -> bool:
-        return abs(self.head.x - self.tail.x) < 2 and abs(self.head.y - self.tail.y) < 2
+                prev_point = point
 
-    def is_tail_in_the_same_row_or_col(self) -> bool:
-        return self.head.x == self.tail.x or self.head.y == self.tail.y
-
-    def __str__(self) -> str:
-        representation = ""
-        for row in self.matrix:
-            for col in row:
-                representation += col
-            representation += "\n"
-
-        return representation
+    def are_points_near(self, point: Point, prev_point: Point) -> bool:
+        return not abs(prev_point.x - point.x) > 1 or abs(prev_point.y - point.y) > 1
 
 
-b = Board(70)
+r = Rope(2)
 for direction, distance in get_order():
-    b.move_rope(direction, distance)
+    r.move_rope(direction, distance)
 
-print("Part 1: ", len(b.tail.traceback))
+print("Part 1: ", len(r.points[-1].traceback))
+
+
+r = Rope(10)
+for direction, distance in get_order():
+    r.move_rope(direction, distance)
+
+print("Part 2: ", len(r.points[-1].traceback))
