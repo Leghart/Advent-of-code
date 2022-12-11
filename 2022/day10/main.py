0681debaf2ef22303aeb2d81f2c857ea4324f2df
Dawid <dawid.sieluzycki@nokia.com>
2022-12-11 11:59:03 +0100

    2022: day10 (part2)

--- 2022/day10/main.py
+++ 2022/day10/main.py
@@ -4,14 +4,17 @@ with open("data.txt", "r") as f:
     data = f.read().splitlines()
     queue = que.Queue()
     register = {}
-    x = 1
+    x = 1  # spirit middle point
     cycle = 1
     timeout = 0
     last_command_idx = -1
+    crt_row = []
+    crt_screen = []
+    screen_width = 40
 
     while True:
 
-        # Finish executing command
+        # Update registry after executing command
         if timeout == 0:
             executing_cmd = False
             last_command_idx += 1
@@ -19,8 +22,18 @@ with open("data.txt", "r") as f:
             if not queue.empty() and timeout == 0:
                 x += queue.get()
 
+        # Change row if is full
+        if len(crt_row) % screen_width == 0:
+            crt_screen.append(crt_row)
+            crt_row = []
+
         # Skip cycle
         if executing_cmd:
+
+            # Draw pixel
+            crt_row.append(
+                "#" if (cycle - 1) % screen_width in (x - 1, x, x + 1) else "."
+            )
             timeout -= 1
             register[cycle] = x
             cycle += 1
@@ -41,7 +54,12 @@ with open("data.txt", "r") as f:
 
         executing_cmd = True
 
-        if cycle > 230:
+        if cycle > 240:
             break
 
+
 print("Part 1: ", sum([cycle * register[cycle] for cycle in range(20, 230, 40)]))
+print("Part 2:")
+for row in crt_screen:
+    if row:
+        print("".join(row))
