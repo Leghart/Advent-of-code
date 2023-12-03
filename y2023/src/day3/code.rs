5e6c1cfb1ec554c831ccf37bcec7e800993cbda2
sieluzyc <dawid.sieluzycki@nokia.com>
2023-12-03 14:48:55 +0100

    day3part2 wip

--- y2023/src/day3/code.rs
+++ y2023/src/day3/code.rs
@@ -1,4 +1,3 @@
-
 #[allow(dead_code)]
 fn load_data() -> Vec<String> {
     std::fs::read_to_string(file!().replace("/code.rs", "/data.txt"))
@@ -7,8 +6,6 @@ fn load_data() -> Vec<String> {
         .map(String::from)
         .collect()
 }
-
-// fn get_bounds()
 
 fn is_symbol_near(f: usize, t: usize, data: &String, size: usize) -> bool {
     let symbols = ['#', '*', '+', '$', '/', '@', '=', '%', '&', '-'];
@@ -109,107 +106,56 @@ pub fn part1() -> Option<usize> {
     Some(result)
 }
 
-
-
-fn asterix_near(f: usize, t: usize, data: &String, size: usize) -> bool {
-    let symbol = &'*';
-    let l_min: usize = 0;
-    let r_max: usize = size - 1;
-
-    //check left & right sides
-    if f % size != l_min {
-        if &data.chars().nth(f - 1).unwrap() == symbol {
-            return true;
-        }
-    }
-    if t % size != r_max {
-        if &data.chars().nth(t).unwrap() == symbol {
-            return true;
-        }
+fn deep_number(idx: usize, data: &String) -> usize {
+    let mut idx_start = idx;
+    while idx_start > 0 && data.chars().nth(idx_start - 1).unwrap().is_digit(10) {
+        idx_start -= 1;
     }
 
-    //check top side
-    if !(l_min..=r_max).contains(&f) {
-        let ff = if (f - size - 1) % size == r_max {
-            f - size
-        } else {
-            f - size - 1
-        };
-
-        let tt = if (t - size + 1) % size == l_min {
-            t - size
-        } else {
-            t - size + 1
-        };
-
-        if data
-            .get(ff..tt)
-            .unwrap()
-            .chars()
-            .any(|c| &c == symbol)
-        {
-            return true;
-        }
+    let mut idx_stop = idx_start;
+    while data.chars().nth(idx_stop + 1).unwrap().is_digit(10) {
+        idx_stop += 1;
     }
 
-    //check bottom side
-    if !(data.len() - size..=data.len()).contains(&f) {
-        let ff = if (f + size - 1) % size == r_max {
-            f + size
-        } else {
-            f + size - 1
-        };
-
-        let tt = if (t + size + 1) % size == l_min {
-            t + size
-        } else {
-            t + size + 1
-        };
-
-        if data
-            .get(ff..tt)
-            .unwrap()
-            .chars()
-            .any(|c| &c == symbol)
-        {
-            return true;
-        }
-    }
-
-    false
+    data.get(idx_start..idx_stop + 1)
+        .unwrap()
+        .parse::<usize>()
+        .unwrap()
 }
 
+fn find_near_numbers(idx: i32, data: &String) -> Vec<usize> {
+    let mut numbers: Vec<usize> = vec![];
+    let directions: [i32; 8] = [-11, -10, -9, -1, 1, 9, 10, 11];
+    for n in directions {
+        let nidx = n + idx;
+        if nidx >= 0
+            && nidx <= data.len() as i32
+            && data.chars().nth(nidx as usize).unwrap().is_digit(10)
+        {
+            let number = deep_number(nidx as usize, &data);
+            if !numbers.contains(&number) {
+                numbers.push(number);
+            }
+        }
+    }
+    numbers
+}
 
 pub fn part2() -> Option<usize> {
     let pure_data = load_data();
-    let size = pure_data.get(0)?.len();
     let data = pure_data.join("");
 
-    let mut steps_to_skip = 0;
-    let mut result:Vec<std::ops::Range<usize>> = vec![];
-    // let a: Range<usize,usize> =
+    let mut result = 0;
 
     for (i, c) in data.chars().enumerate() {
-        let _from: usize = i;
-        let mut _to: usize = i;
-
-        if steps_to_skip > 0 {
-            steps_to_skip -= 1;
-            continue;
-        }
-
-        if c.is_digit(10) {
-            while data.chars().collect::<Vec<char>>()[_to].is_digit(10) {
-                _to += 1;
-            }
-            steps_to_skip = _to - _from;
-
-
-            if asterix_near(_from, _to, &data, size) {
-                result.push(_from.._to);
+        if c == '*' {
+            let numbers = find_near_numbers(i as i32, &data);
+            println!("{:?}", numbers);
+            if numbers.len() == 2 {
+                result += numbers.iter().product::<usize>();
             }
         }
-    }   
-    println!("{:?}",result) ;
-    None
+    }
+
+    Some(result)
 }
